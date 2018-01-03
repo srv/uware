a7d2dcb8c384f7e181c4ad950d43e0af673ab301
Miquel Massot <miquel.massot@gmail.com>
2018-01-03 09:44:53 +0100

    Release for Kinetic

--- include/preprocess.h
+++ include/preprocess.h
@@ -23,8 +23,8 @@
 #include <pcl/filters/crop_box.h>
 
 #include <opencv2/opencv.hpp>
-#include <opencv2/features2d/features2d.hpp>
-#include <opencv2/nonfree/features2d.hpp>
+#include <opencv2/features2d.hpp>
+#include <opencv2/xfeatures2d.hpp>
 
 #include <boost/filesystem.hpp>
 #include <boost/lexical_cast.hpp>
