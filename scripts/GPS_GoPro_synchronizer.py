3fcd239c4f2c7244ec83d2d9a5263cd68d6c5db9
Bo Miquel Nordfeldt <bomiquel@gmail.com>
2022-05-10 12:35:49 +0200

    Script to synchronize GoPro images with GPS position

---
+++ scripts/GPS_GoPro_synchronizer.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+import pyexiv2
+import pandas as pd
+
+
+class Synchronizer:
+
+
+    def __init__(self, inI, inGPS, outCSV):
+
+        # Declare paths.
+        self.inputImage = inI
+        self.inputGPS = inGPS
+        self.outputCSV = outCSV
+
+        # Determine if the output files exist. 
+        if os.path.isdir( self.outputCSV):
+            if not "/" in self.outputCSV[-1]:
+                self.outputCSV += "/"
+            self.exist = False
+            for file in sorted(os.listdir(self.outputCSV)):
+                if (".csv" in file) and not ((self.outputCSV + file) is inGPS):
+                    self.outputDataFrame = pd.read_csv(self.outputCSV + file, sep = ",")
+                    self.exist = True
+                    self.outputCSV += file
+                    break
+            if not self.exist:
+                self.outputDataFrame = pd.DataFrame(columns = ['#id', 'Latitude', 'Longitude', 'Timestamp'])
+        elif os.path.isfile(self.outputCSV) and (".csv" in self.outputCSV):
+            self.outputDataFrame = pd.read_csv(self.outputCSV, sep = ",")
+            self.exist = True
+
+
+    # def fpsDeterminer(self):
+    #     print()
+
+    def synchronize(self):
+
+        # Obtain image name.
+        idImage = self.inputImage.split("/")
+        idImage = idImage[-1]
+        print(idImage)
+
+        # Obtain image time stamp from image metadata.
+        metadata = pyexiv2.ImageMetadata(self.inputImage)
+        metadata.read()
+        for exif_key in metadata.exif_keys:
+            if "Image.DateTime" in exif_key:
+                timeImage = metadata[exif_key].raw_value.split(" ")
+                timeImage = timeImage[-1]
+
+        # Read CSV with GPS locations, then clean the dataset.
+        if ".LLH" in self.inputGPS:
+            df = pd.read_csv(self.inputGPS, sep = " ")
+            df = df.dropna(axis = 1)
+            df.columns = ['Date', 'Timestamp', 'Latitude', 'Longitude', 'Height', 'Random 1', 'Random 2']
+        elif ".csv" in self.inputGPS:
+            df = pd.read_csv(self.inputGPS, sep = ",")
+
+        # Determine the miliseconds of the image, depends on the fps
+        timestamps = list(df[df['Timestamp'].str.contains(timeImage)]['Timestamp'])
+        if len(self.outputDataFrame) > 0:
+            if not self.outputDataFrame['Timestamp'].str.contains(timestamps[0]).any():
+                timeImage = timestamps[0]
+            else:
+                timeImage = timestamps[3]
+        else:
+            timeImage = timestamps[0]
+        print(timeImage)
+
+        # Obtain latitude and longitude of the image.
+        latitudeImage = float(df[df['Timestamp'] == timeImage]['Latitude'])
+        longitudeImage = float(df[df['Timestamp'] == timeImage]['Longitude'])
+
+        # Add new row to the dataset and write it to a file.
+        add_values = {'#id': idImage, 
+                      'Latitude': latitudeImage,
+                      'Longitude': longitudeImage,
+                      'Timestamp': timeImage}
+        add_row = pd.Series(add_values)
+        self.outputDataFrame = self.outputDataFrame.append(add_row, ignore_index = True)
+        if self.exist:
+            self.outputDataFrame.to_csv(path_or_buf = self.outputCSV, sep = ",", index = False)
+        else:
+            self.outputDataFrame.to_csv(path_or_buf = self.outputCSV + "image_latlon_2.csv", sep = ",", index = False)
+
+        print("---------------------------------------------------------------------------")
+
+
+if __name__ == '__main__':
+
+    # Read from the command line.
+    parser = argparse.ArgumentParser(description = "")
+    parser.add_argument("--inImage", help = "Path to .jpg or path to the directory with .jpg")
+    parser.add_argument("--inGPS", help = "Path to GPS.csv")
+    parser.add_argument("--outCSV", default = os.getcwd(), help = "Path to save .csv file with images and lat-lng")
+    args = parser.parse_args()
+
+    # Sanity check.
+    if os.path.exists(args.inImage) and os.path.exists(args.inGPS):
+
+        # Synchronize one image.
+        if os.path.isfile(args.inImage) and os.path.isfile(args.inGPS):
+            sync = Synchronizer(args.inImage, args.inGPS, args.outCSV)
+            sync.synchronize()
+
+        # Synchronize some images.
+        elif os.path.isdir(args.inImage) and os.path.isfile(args.inGPS):
+            for file in sorted(os.listdir(args.inImage)):
+                image = args.inImage + "/" + file
+                if os.path.isfile(image) and (".JPG" in image):
+                    sync = Synchronizer(image, args.inGPS, args.outCSV)
+                    sync.synchronize()
+                else:
+                    print("Problems with the image")
+
+    else:
+        print("Input paths don't exists")
--- scripts/jpg2geotiff.py
+++ scripts/jpg2geotiff.py
@@ -7,12 +7,12 @@ from pathlib import Path
 from PIL import Image
 from osgeo import gdal, osr
 
-
-class jpg2geotiff:
+class Jpg2Geotiff:
 
 
     def __init__(self, inI, inCSV, outI):
 
+        # Declare paths.
         self.inputImage = inI
         self.inputGeocsv = inCSV
         self.outputImage = outI
@@ -26,38 +26,43 @@ class jpg2geotiff:
 
     def tiff2geotiff(self):
 
+        # Get the image name frome the input path.
         auxImagename = self.inputImage.split("/")
         auxImagename = auxImagename[-1].split(".")
+
+        # Dataset with all the latitude and longitude coordinates.
         df = pd.read_csv(self.inputGeocsv, sep = ",", index_col=False)
         columns = list(df.columns)
         corners = {}
 
-        print(auxImagename[0] == "000000")
-
+        # Remove all leading zeros on the image name.
         if "000000" in auxImagename[0]:
             imageName = 0
-        
         else:
             imageName = int(auxImagename[0].lstrip('0'))
         
+        # Get the latitude and longitude, from the dataset with all the latitude
+        # and longitude coordinates, of all the corners of the image.
         for column in columns:
             if ("lat" in column) or ("lon" in column):
                 corners[column] = float(df[df[columns[0]] == imageName][column])
 
         ds = gdal.Open(self.outputImage, gdal.GA_Update)
 
+        # Get raster dimensions
         width = ds.RasterXSize
         height = ds.RasterYSize
 
+        # Set src
         src = osr.SpatialReference()
         src.ImportFromEPSG(4326) 
 
+        # Set latitude and longitude to all image corners
         gcps = [
         gdal.GCP(corners['uprightlon'], corners['uprightlat'], 0, width, 0),
         gdal.GCP(corners['upleftlon'], corners['upleftlat'], 0, 0, 0),
         gdal.GCP(corners['downrightlon'], corners['downrightlat'], 0, width, height),
         gdal.GCP(corners['downleftlon'], corners['downleftlat'], 0, 0, height)]
-
         ds.SetGCPs(gcps, src.ExportToWkt())
 
         # ds = None
@@ -65,6 +70,7 @@ class jpg2geotiff:
 
 if __name__ == '__main__':
 
+    # Read from the command line
     parser = argparse.ArgumentParser(description = "")
     parser.add_argument("--inImage", help = "Path to .jpg or path to the directory with .jpg")
     parser.add_argument("--inCSV", help = "Path to .csv with the lat-lng of the image corners")
@@ -75,33 +81,33 @@ if __name__ == '__main__':
     inLATLON = Path(args.inCSV)
     outI = Path(args.outImage)
 
-    # Sanity checks
+    # Sanity check
     if (inI.exists() and inLATLON.exists()):
 
+        # Sanity check
         if not outI.exists():
             os.makedirs(args.outImage)
 
+        # Sanity check
         if not (args.outImage[-1] is "/"):
             args.outImage = args.outImage + "/"
 
+        # Transform one .JPG
         if inI.is_file():
-            j2gt = jpg2geotiff(args.inImage, args.inCSV, args.outImage + inI.stem + ".tiff")
+            j2gt = Jpg2Geotiff(args.inImage, args.inCSV, args.outImage + inI.stem + ".tiff")
             j2gt.jpg2tiff()
             j2gt.tiff2geotiff()
 
+        # Transform some .JPG
         elif inI.is_dir():
-
             if not (args.inImage[-1] is "/"):
                 args.inImage = args.inImage + "/"
-
             files = sorted(os.listdir(args.inImage))
-
             for file in files:
                 print(file)
-
                 if ".jpg" in file:
                     auxFile = file.split(".")
-                    j2gt = jpg2geotiff(args.inImage + file, args.inCSV, args.outImage + auxFile[0] + ".tiff")
+                    j2gt = Jpg2Geotiff(args.inImage + file, args.inCSV, args.outImage + auxFile[0] + ".tiff")
                     j2gt.jpg2tiff()
                     j2gt.tiff2geotiff()
 
